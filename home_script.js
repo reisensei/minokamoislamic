ef5515b39106c5d92c074ce191333eb26baeb8b2
Rei Sensei <51923332+reisensei@users.noreply.github.com>
2025-04-24 21:11:59 +0900

    Add files via upload

--- home_script.js
+++ home_script.js
@@ -1,6 +1,17 @@
+
 const menu = document.querySelector('nav ul');
 const menuOpen = document.querySelector('.menu-open');
 const menuClose = document.querySelector('.donate-cta');
 
 menuOpen.addEventListener('click', () => { menu.classList.add('open') });
 menuClose.addEventListener('click', () => { menu.classList.remove('open') });
+
+
+function copyText(elementId) {
+    const text = document.getElementById(elementId).innerText;
+    navigator.clipboard.writeText(text).then(() => {
+        alert("Copied: " + text);
+    }).catch(err => {
+        alert("Failed to copy: " + err);
+    });
+}
